eef3305686c663a39437885fdb53b8f28de24a89
Filipemtb <filipera3@gmail.com>
2023-05-23 16:11:27 -0300

    refactoring

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router} = require("express"); 
 
-const UsersController = require("..//controllers/UsersController");
+const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const userRouter = Router();
 
 const usersController = new UsersController();
 
 userRouter.post("/", usersController.create);
-userRouter.put("/:id", usersController.update);
+userRouter.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = userRouter;   // Exporting to those who want to use this file can use.
