8ee6ecb02cfd0ce1ba9490a82a66da98951f9912
Filipemtb <filipera3@gmail.com>
2023-05-29 15:33:20 -0300

    creating function to save and function to delete the image

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,28 @@
+const fs = require('fs'); 
+const path = require('path'); 
+const uploadConfig = require("../config/upload");
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promisses.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    );
+
+    return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    }catch {
+      return false;
+    }
+
+    await fs.promises.unlink(file);
+  }
+}
+
+module.exports = DiskStorage;
