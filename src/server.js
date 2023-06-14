426810d72f33f5c5411cd0df7ea3cd9963730a7a
Filipemtb <filipera3@gmail.com>
2023-06-14 15:07:38 -0300

    initializing cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations");
-
 const AppError = require("./utils/AppError");
-
 const uploadConfig = require("./config/upload");
 
-const express = require('express');
-
-const routes = require('./routes') // Import routes from index.js
+const cors = require("cors");
+const express = require("express");
+const routes = require("./routes") // Import routes from index.js
 
 migrationsRun();
 
 const app = express(); 
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
